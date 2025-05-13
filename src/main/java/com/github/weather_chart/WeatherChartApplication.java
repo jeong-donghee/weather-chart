552dedcddb7524987239d05ef77af7a4c4ce040c
jeongdonghee <weg9511@gmail.com>
2025-05-13 09:04:23 +0900

    init project

---
+++ src/main/java/com/github/weather_chart/WeatherChartApplication.java
@@ -0,0 +1,13 @@
+package com.github.weather_chart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeatherChartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeatherChartApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/weather_chart/WeatherChartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.weather_chart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeatherChartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
